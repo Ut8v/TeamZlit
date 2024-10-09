cc1fe054a5029596c4833863faadb50a5490a5c0
utshav khatiwada <utshavkhatiwada4@gmail.com>
2024-10-09 19:53:34 -0400

    nav bar created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,22 @@
-
-import './App.css'
+import NavigationBar from './components/nav-bar';
+import './App.css';
+import './styles/nav-bar.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Page1 from './pages/Page1';
 
 function App() {
 
   return (
     <>
-     <p>Team Z Lit</p>
+    <NavigationBar />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}>
+        </Route>
+        <Route path="/page1" element={ <Page1/> } />
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
---
+++ client/src/components/nav-bar.jsx
@@ -0,0 +1,18 @@
+import { Navbar, Nav } from "react-bootstrap"
+import logo from "../assets/logo.png";
+
+const NavigationBar = () => {
+    return(
+        <Navbar className="navbar-container d-flex flex-column align-items-start">
+        <Navbar.Brand>
+          <img src={logo} alt="Logo" className="d-inline-block align-top logo-pic" />
+        </Navbar.Brand>
+        <Nav className="nav-bar">
+          <Nav.Link href="/" className="nav-link">Home</Nav.Link>
+          <Nav.Link href="/page1" className="nav-link">Page 1</Nav.Link>
+        </Nav>
+      </Navbar>
+    )
+}
+
+export default NavigationBar;
