059a2464fa491ccab75e0f54e688daa0af973959
danny <40042446+xxxlaf@users.noreply.github.com>
2024-10-23 16:33:43 -0400

    added more fields

--- client/src/components/ui/formToCreateTeam.jsx
+++ client/src/components/ui/formToCreateTeam.jsx
@@ -1,8 +1,11 @@
 
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
+import { format } from "date-fns";
 import { z } from "zod";
+import { Calendar as CalendarIcon } from "lucide-react"
 
+import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import {
   Form,
@@ -14,12 +17,34 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { Checkbox } from "@/components/ui/checkbox"
+import { Checkbox } from "@/components/ui/checkbox";
+import { Textarea } from "@/components/ui/textarea";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
+import { Calendar } from "@/components/ui/calendar"
+
+// We'll want to later pull these options from a table
+const roleOptions = ["Developer", "Designer", "Manager"];
 
 const formSchema = z.object({
-  teamName: z.string().min(2, {
-    message: "Team Name must be at least 3 characters.",
-  }),
+  teamName: z.string().min(3, { message: "Team Name must be at least 3 characters.", }),
+  teamDescription: z.string().min(2, { message: "Team Description must be at least 2 characters.", }),
+  date: z.object({
+    from: z.date(),
+    to: z.date(),
+  })
 });
 
 export function FormToCreateTeam() {
@@ -27,8 +52,11 @@ export function FormToCreateTeam() {
     resolver: zodResolver(formSchema),
     defaultValues: {
       teamName: "",
+      teamDescription: "",
     },
   });
+
+  
 
   function onSubmit(values) {
     console.log(values);
@@ -44,17 +72,80 @@ export function FormToCreateTeam() {
             <FormItem>
               <FormLabel>Team Name</FormLabel>
               <FormControl>
-                <Input placeholder="this is placeholder text" {...field} />
+                <Input 
+                  placeholder="Capstone Team" {...field} 
+                  className="w-[360px]" 
+                />
               </FormControl>
-              <FormDescription>
-                This is just a description
-              </FormDescription>
               <FormMessage />
             </FormItem>
           )}
         />
+
+        <FormField
+          control={form.control}
+          name="teamDescription"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Team Description</FormLabel>
+              <FormControl>
+                <Textarea 
+                  placeholder="A team to develop a web application" {...field} 
+                  className="w-[360px]"
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="teamType"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Team Type</FormLabel>
+              <FormControl>
+              <Select>
+                <SelectTrigger className="w-[360px]">
+                  <SelectValue placeholder="Select a team type" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectItem value="developmentteam">Development Team</SelectItem>
+                    <SelectItem value="researchgroup">Research Group</SelectItem>
+                    <SelectItem value="studygroup">Study Group</SelectItem>
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="skillsRequired"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Skills Required</FormLabel>
+              <FormControl>
+                <div className="flex items-center space-x-2">
+                  <Checkbox id="javascript" />
+                  <label
+                    htmlFor="terms"
+                    className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70" >
+                    JavaScript
+                  </label>
+                </div>
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
         <Button type="submit">Submit</Button>
-        <Checkbox />
       </form>
     </Form>
   );
