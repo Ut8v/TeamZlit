23c2182b86526b68ed482d27e90c4ed1903df269
danny <40042446+xxxlaf@users.noreply.github.com>
2025-02-07 09:15:49 -0500

    Changed look slightly

--- client/src/components/nav-bar.jsx
+++ client/src/components/nav-bar.jsx
@@ -14,16 +14,13 @@ const NavigationBar = () => {
         <Link to="/" className="nav-link">Home</Link>
       </li>
       <li className="list-group-item">
-        <Link to="/teamIndex" className="nav-link">Team Index</Link>
-      </li>
-      <li className="list-group-item">
-        <Link to="/profile" className="nav-link">Profile</Link>
-      </li>
-      <li className="list-group-item">
         <Link to="/findTeam" className="nav-link">Find Team</Link>
       </li>
       <li className="list-group-item">
         <Link to="/createTeam" className="nav-link">Create Team</Link>
+      </li>
+      <li className="list-group-item">
+        <Link to="/teamIndex" className="nav-link">Browse Teams</Link>
       </li>
     </ul>
   </Nav>
