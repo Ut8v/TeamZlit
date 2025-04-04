ce411d0c96f2db9e9180975e2928b88732d2d2f8
Utshav Khatiwada <utshavkhatiwada@MacBook-Pro-178.lan>
2025-04-04 15:45:18 -0400

    changes on config to redirect page to correct page

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path';
-import { fileURLToPath } from 'url';
+import path from 'path'
+import { fileURLToPath } from 'url'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'public/_redirects',
+          dest: '.',
+        },
+      ],
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
-});
+})
