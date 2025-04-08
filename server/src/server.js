dfc6f28fb33fde72c45f2a913814f9a6b13902e9
Utshav Khatiwada <utshavkhatiwada@MacBook-Pro-178.lan>
2025-04-07 21:01:37 -0400

    proxy error on the server

--- server/src/server.js
+++ server/src/server.js
@@ -26,6 +26,8 @@ const membersRoutes = require('./routes/findTeam/members');
 const rateLimit = require('express-rate-limit');
 const authenticateUser = require('./authMiddleware/authMiddleware');
 
+app.set('trust proxy', true);
+
 app.use(cors({
     origin: process.env.URL, 
     methods: ['GET','PUT', 'POST', 'DELETE'],
